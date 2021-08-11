7d5756419e975465f628879107da29ca16926523
Jose Lerma <pepelerma.aguilera@gmail.com>
2021-08-10 22:28:58 -0500

    fixing something in production

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
 const Categoria = require("./categoria");
 const Usuario = require("./usuario");
-const Server = require("./server");
+//const Server = require("./server");
 const Role = require("./role");
 const Producto = require("./producto");
 
 module.exports = {
   Categoria,
   Usuario,
-  Server,
+  //Server,
   Role,
   Producto,
 };
